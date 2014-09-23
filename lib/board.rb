a6586fa74672ba5bf9fadae6b06d9406635fdc86
Camilla van Klinken <camillavk@gmail.com>
2014-09-23 17:48:57 +0100

    Board module added: able to access individual squares and change them to occupied

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+module Board
+
+attr_accessor :grid
+
+	def initialize
+		@grid = ("A".."J").map{|x| (1..10).map {|y| x + y.to_s}}
+	end
+
+	def grid
+		@grid
+	end
+
+	def square(x,y)
+		@grid [x][y]
+	end
+
+	def set_square(x,y)
+		grid[x][y] = "occupied"
+	end
+
+
+end

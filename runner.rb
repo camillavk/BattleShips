8c7209477394ab7bdcbe6767759999faec3c4818
Camilla van Klinken <camillavk@gmail.com>
2014-09-25 11:44:43 +0100

    runner file added

---
+++ runner.rb
@@ -0,0 +1,8 @@
+require './lib/board.rb'
+require './lib/cell.rb'
+require './lib/water.rb'
+require './lib/ships.rb'
+require './lib/player_board.rb'
+require './lib/opponent_board.rb'
+require './lib/player.rb'
+

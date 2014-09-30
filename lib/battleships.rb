3019e2c77a83d26c1aa438e329e84dcbe2bdf44a
Hadi <hadichalabi@gmail.com>
2014-09-30 17:17:37 +0100

    Ships Refactored

---
+++ lib/battleships.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+
+class BattleShips < Sinatra::Base
+
+	set :views, Proc.new { File.join(root, ".." , "views") }
+  get '/' do
+    'Hello BattleShips!'
+    erb :index
+  end
+
+  get '/newgame' do
+  	'New Game'
+  	erb :index
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
